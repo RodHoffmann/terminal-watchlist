eabe4014c279c1a44480820c63a62ec8e7ba911d
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-01-02 11:24:05 +0100

    Movie Model Completed

---
+++ models/movie.rb
@@ -0,0 +1,11 @@
+class Movie
+  attr_accessor :id
+  attr_reader :name, :rating, :year
+
+  def initialize(attr = {})
+    @id = attr[id]
+    @name = attr[name]
+    @rating = attr[rating]
+    @year = attr[year]
+  end
+end
